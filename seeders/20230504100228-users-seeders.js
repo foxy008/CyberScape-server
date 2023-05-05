d330eff15ac94ccc8197f479d56da6a2fdd95249
AshlihSyahrulG <ashlihsyahrulg@gmail.com>
2023-05-05 16:36:09 +0700

    refactor: add seeders data

--- seeders/20230504100228-users-seeders.js
+++ seeders/20230504100228-users-seeders.js
@@ -1,25 +1,24 @@
 'use strict';
+const { hashPass } = require("../helpers/bcrypt")
+const fs = require ("fs")
+
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    let data = JSON.parse(fs.readFileSync("./data/users.json", "utf-8"))
+    // console.log(data);
+    data.forEach((el) => {
+      el.password = hashPass(el.password)
+      el.createdAt = new Date(),
+      el.updatedAt = new Date()  
+      return el      
+    });
+    await queryInterface.bulkInsert('Users', data)
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Users', null, {});
+
   }
 };
--- seeders/20230504100336-nfts-seeders.js
+++ seeders/20230504100336-nfts-seeders.js
@@ -1,25 +1,21 @@
 'use strict';
+const fs = require ("fs")
+
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    let data = JSON.parse(fs.readFileSync("./data/NFTs.json", "utf-8"))
+    // console.log(data);
+    data.forEach((el) => {
+      el.createdAt = new Date(),
+      el.updatedAt = new Date()   
+      return el     
+    });
+    await queryInterface.bulkInsert('NFTs', data)
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('NFTs', null, {});
   }
 };
