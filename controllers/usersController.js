2ab92b55ddb7be5faae52ac3a42b034a9c816c12
Edward <edw26alex@protonmail.com>
2023-05-05 15:28:26 +0700

    refactor: routes added with endpoints

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,27 @@
 class usersController {
+    static async register(req, res, next) {
+        try {
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    static async login(req, res, next) {
+        try {
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    static async getProfile(req, res, next) {
+        try {
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
 }
 
--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,11 @@ const roomsRouter = require('./users');
 const NFTsRouter = require('./users');
 const ratingsRouter = require('./users');
 const userFavoritesRouter = require('./users');
-const roomNFTsRouter = express.Router();
 
 router.use('/users', usersRouter);
 router.use('/rooms', roomsRouter);
 router.use('/NFTs', NFTsRouter);
 router.use('/ratings', ratingsRouter);
 router.use('/favorites', userFavoritesRouter);
-router.use('/room-nfts', roomNFTsRouter);
 
 module.exports = router;
