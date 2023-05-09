93c1101639a999a296cc152aec20210e84511b5f
AshlihSyahrulG <ashlihsyahrulg@gmail.com>
2023-05-09 13:03:15 +0700

    fix: test

---
+++ models/log.js
@@ -0,0 +1,34 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Log extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Log.belongsTo(models.User)
+    }
+  }
+  Log.init({
+    UserId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Users',
+        key: 'id'
+      }
+    },
+    transactionId: DataTypes.INTEGER,
+    status:{
+      type: DataTypes.STRING,
+      defaultValue : "pending"
+    }
+  }, {
+    sequelize,
+    modelName: 'Log',
+  });
+  return Log;
+};
