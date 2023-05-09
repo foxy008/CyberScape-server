4fd24c6ca780e4113fb22b665904e1b7a5b099dd
DESKTOP-LBBL8CS\Fitra <justfitra11@gmail.com>
2023-05-09 17:36:53 +0700

    feat: update news controller

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,20 +1,25 @@
-const NewsAPI = require('newsapi')
+const NewsAPI = require("newsapi");
 const newsapi = new NewsAPI("a46249efc4984d83b05016fb1c6118f5");
 
 class newsController {
-    static async getNFTNews(req, res, next) {
-        try {
-            const news = await newsapi.v2.everything({
-                q: "NFT",
-                from: "2022-26-05",
-                sortBy: "popularity"
-            })
+  static async getNFTNews(req, res, next) {
+    try {
+      const date = new Date();
+      date.setDate(date.getDate() - 1);
+      const formattedDate = date.toISOString().split("T")[0];
 
-            res.status(200).json(news)
-        } catch (error) {
-            next(error)
-        }
+      const news = await newsapi.v2.everything({
+        q: "NFT",
+        from: formattedDate,
+        sortBy: "popularity",
+        pageSize: 100,
+      });
+
+      res.status(200).json(news);
+    } catch (error) {
+      next(error);
     }
+  }
 }
 
-module.exports = newsController
+module.exports = newsController;
